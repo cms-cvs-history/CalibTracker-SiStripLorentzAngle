682f3340614cb150d471cf10c0878d72f33c61c2
Chiara Genta <sha1-5a1ddac03dd3e96834db2f498eca6aad5396cb8e@cern.ch>
2007-01-26 10:35:18 +0000

    removed unnecessary files and updated cfg

--- test/SealModules.cc
+++ test/SealModules.cc
@@ -1,6 +1,4 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "CalibTracker/SiStripLorentzAngle/test/AnalyzeTracks.h"
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(AnalyzeTracks);
 
